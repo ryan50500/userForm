07562b42ec80cc6171195690d3e95bcc1c14eea6
ryan woods <federer5050@gmail.com>
2020-09-10 17:58:59 +0100

    adding readme and js event listeners

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# userForm
+Project can be found at https://ryan50500.github.io/userForm/index.html
---
+++ app.js
@@ -0,0 +1,21 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('Phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+
+}
+
+function validateZip() {
+    
+}
+
+function validateEmail() {
+    
+}
+
+function validatePhone() {
+    
+}
