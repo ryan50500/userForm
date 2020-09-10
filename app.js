d4905221bc5a9b8b2cbde0f6bc198ccfe816844a
ryan woods <federer5050@gmail.com>
2020-09-10 18:09:39 +0100

    first validation

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('Phone').addEventListener('blur', validatePhone);
 
 function validateName() {
+    const name = document.getElementById('name');
+    const re = /^[a-zA-Z]{2,10}$/;
+    // ^ means starts with a-z or A-Z, and between 2 and 10 characters
 
+    if(!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.remove('is-invalid');
+    }
 }
 
 function validateZip() {
