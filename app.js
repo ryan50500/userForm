70f2e503222925ae3881b3e920355701aab9195f
ryan woods <federer5050@gmail.com>
2020-09-11 12:35:03 +0100

    adding UK mobile validation

--- app.js
+++ app.js
@@ -5,45 +5,47 @@ document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
 function validateName() {
-  const name = document.getElementById('name');
-  const re = /^[a-zA-Z]{2,10}$/;
+    const name = document.getElementById('name');
+    const re = /^[a-zA-Z ]{2,15}$/;
+    // ^ means starts with a-z or A-Z, and between 2 and 10 characters
 
-  if(!re.test(name.value)){
-    name.classList.add('is-invalid');
-  } else {
-    name.classList.remove('is-invalid');
-  }
+    if(!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.remove('is-invalid');
+    }
 }
 
 function validateZip() {
-  const zip = document.getElementById('zip');
-  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+    const zip = document.getElementById('zip');
+    const re = /^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/;
+    // ^ means starts with 0-9
 
-  if(!re.test(zip.value)){
-    zip.classList.add('is-invalid');
-  } else {
-    zip.classList.remove('is-invalid');
-  }
+    if(!re.test(zip.value)) {
+        zip.classList.add('is-invalid');
+    } else {
+        zip.classList.remove('is-invalid');
+    }
 }
 
 function validateEmail() {
-  const email = document.getElementById('email');
-  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    const email = document.getElementById('email');
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
-  if(!re.test(email.value)){
-    email.classList.add('is-invalid');
-  } else {
-    email.classList.remove('is-invalid');
-  }
+    if(!re.test(email.value)) {
+        email.classList.add('is-invalid');
+    } else {
+        email.classList.remove('is-invalid');
+    }
 }
 
 function validatePhone() {
-  const phone = document.getElementById('phone');
-  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
-
-  if(!re.test(phone.value)){
-    phone.classList.add('is-invalid');
-  } else {
-    phone.classList.remove('is-invalid');
+    const phone = document.getElementById('phone');
+    const re = /^(\+44\s?\d{10}|0044\s?\d{10}|0\s?\d{10})?$/;
+  
+    if(!re.test(phone.value)){
+      phone.classList.add('is-invalid');
+    } else {
+      phone.classList.remove('is-invalid');
+    }
   }
-}
